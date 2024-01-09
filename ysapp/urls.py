f4c280196fea1238078dcb8b014280ed44305ee5
yigitcan <yigitcanakcora@gmail.com>
2024-01-09 23:07:56 +0300

    url degisimi

--- ysapp/urls.py
+++ ysapp/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'ysapp'
 urlpatterns = [
     path('', anasayfa, name="anasayfa"),
-    # path('', restoran, name='restoranlar'),
-    # path('restoranlar/<int:pk>/', restoranlar.as_view(), name="restorandetay"),
+    # path('restoranlar/', RestoranListView.as_view(), name='restoranlar'),
+    # path('restoranlar/<int:pk>/', RestoranDetailView.as_view(), name="restorandetay"),
 ] 
