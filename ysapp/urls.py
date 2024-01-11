299a4365a1783a9dbc55edb3c948fb9436a2240e
yigitcan <yigitcanakcora@gmail.com>
2024-01-12 00:43:41 +0300

    restoran ekleme

--- ysapp/urls.py
+++ ysapp/urls.py
@@ -4,10 +4,10 @@ from .views import *
 app_name = 'ysapp'
 urlpatterns = [
     path('', anasayfa, name="anasayfa"),
-    # path('restoranlar/', RestoranListView.as_view(), name='restoranlar'),
-    # path('restoranlar/<int:pk>/', RestoranDetailView.as_view(), name="restorandetay"),
-    # path('restoranlar/olustur/', RestoranCreateView.as_view(), name="restoranolustur"),
-    # path('restoranlar/<int:pk>/yemek/', YemekAddView.as_view(), name= 'yemekadd')
+    path('restoranlar/', RestoranListView.as_view(), name='restoranlar'),
+    path('restoranlar/<int:pk>/', RestoranDetailView.as_view(), name="restorandetay"),
+    path('restoranlar/olustur/', RestoranCreateView.as_view(), name="restoranolustur"),
+    path('restoranlar/<int:pk>/yemek/', YemekAddView.as_view(), name= 'yemekadd')
 
 
 
